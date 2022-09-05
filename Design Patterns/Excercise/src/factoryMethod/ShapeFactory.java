a9ded6a6c446bd1a94fa9b2fd8aeba0bc7ad5918
Compass <ndphong165@gmail.com>
2022-09-05 15:44:20 +0700

    Factory Excercise

---
+++ Design Patterns/Excercise/src/factoryMethod/ShapeFactory.java
@@ -0,0 +1,10 @@
+package factoryMethod;
+
+
+public class ShapeFactory {
+    public Shape getShape(String a) {
+        if (a.equals("C")) return new Circle();
+        else if (a.equals("S")) return new Square();
+        else return new Rectangle();
+    }
+}
---
+++ Design Patterns/Excercise/src/factoryMethod/Test.java
@@ -0,0 +1,16 @@
+package factoryMethod;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap vao 1 ky tu C/S/R: ");
+        String cha = scanner.next();
+        Shape s1 = shapeFactory.getShape(cha);
+        s1.draw();
+    }
+
+}
